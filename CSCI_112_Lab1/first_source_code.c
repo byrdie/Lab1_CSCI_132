31001b503226955094965d6bd7291462a766ae85
roy.smart <roy.smart@cslab5.eps.montana.edu>
2014-05-13 11:24:17 -0600

    First commit

---
+++ CSCI_112_Lab1/first_source_code.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: roy.smart
+ *
+ * Created on May 13, 2014, 11:16 AM
+ */
+
+#include <stdio.h>      //include code from the standard IO library
+#include <stdlib.h>     //include code from the standard library
+
+/*
+ * First C program, this a comment that does not get translated by the compiler
+ */
+int main(int argc, char** argv) {
+    
+    puts("Roy Smart wrote this code");
+    
+    return 0;
+}
+
